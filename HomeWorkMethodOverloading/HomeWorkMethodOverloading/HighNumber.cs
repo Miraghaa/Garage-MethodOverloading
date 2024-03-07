58c3bcf44e0f58376b341c7c80f19c684f71ba71
Miraga99 <miraga041@gmail.com>
2024-03-07 19:17:20 +0400

    Success

---
+++ HomeWorkMethodOverloading/HomeWorkMethodOverloading/HighNumber.cs
@@ -0,0 +1,32 @@
+﻿namespace HomeWorkMethodOverloading;
+
+public class HighNumber
+{
+    public int HighResult(int number1, int number2, int number3)
+    {
+        if (number1 > number2 && number1 > number3)
+        {
+            return number1;
+        }
+        else if (number2 > number1 && number2 > number3)
+        {
+            return number2;
+        }
+        else
+        {
+            return number3;
+        }
+    }
+    public int HighResult(int number1, int number2, int number3, int number4)
+    {
+        int result = HighResult(number1, number2, number3);
+        if (number4 > result) return number4;
+        else return result;
+    }
+    public int HighResult(int number1, int number2, int number3, int number4, int number5)
+    {
+        int result = HighResult(number1, number2, number3, number4);
+        if (number5 > result) return number5;
+        else return result;
+    }
+}
---
+++ HomeWorkMethodOverloading/HomeWorkMethodOverloading/Program.cs
@@ -0,0 +1,5 @@
+﻿using HomeWorkMethodOverloading;
+
+HighNumber highNumber = new HighNumber();
+
+Console.WriteLine(highNumber.HighResult(4,5,10,7,8));
